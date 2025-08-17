6c572000d6069b1a81e0940b057b0b5e829bdbd4
anonymous021-hub <komail316860@gmail.com>
2025-08-17 22:07:21 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~k2cVnKgb#BGV2mXK36ML5FF_CdO_9jJnWZOJ5cDSDXL1dSVvFsr4",  // add your Session Id here
+  SESSION_ID: "malvin~snF11TQR#C8SyVmOzTK6czkO6oIR3tvdFQEvtrbhcK_JiAoSZXps",  // add your Session Id here
   
   OWNER_NUMBER: "923092762597", // put your phone number here
   
