f2b5253a66ab61d19229fde4dfa7b2783c8d5580
anonymous021-hub <komail316860@gmail.com>
2025-08-17 21:50:50 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~k2cVnKgb#BGV2mXK36ML5FF_CdO_9jJnWZOJ5cDSDXL1dSVvFsr4",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923092762597", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
